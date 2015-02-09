4a6c53a6545d3eee5623f3fa278ef6798463a5c9
Steffen Schalck <sts@schultz.dk>
2015-02-09 15:33:52 +0100

    Initial structure

---
+++ Client/Client.Droid/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using ClientForms;
+
+namespace Client.Droid
+{
+    [Activity(Label = "Client", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
